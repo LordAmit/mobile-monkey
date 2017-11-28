c91f19d7c8557f2ac9bfec74cd11eaef37141268
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-11-28 06:48:29 +0600

    minor changes

--- test.py
+++ test.py
@@ -12,13 +12,13 @@ from threading import Thread
 from adb_settings import KeyboardEvent
 import enum
 
-def __random_value_generator(lower_limit: int, upper_limit: int):
+''' def __random_value_generator(lower_limit: int, upper_limit: int):
 
     if not isinstance(lower_limit, int):
         raise ValueError("lower_limit must be int")
     if not isinstance(upper_limit, int):
         raise ValueError("upper_limit must be int")
-    return random.randint(lower_limit, upper_limit)
+    return random.randint(lower_limit, upper_limit) '''
 
 def some_method() -> str:
 
@@ -26,12 +26,10 @@ def some_method() -> str:
     emulator = emulator_manager.get_adb_instance_from_emulators(config.EMULATOR_NAME)
     adb_settings = AdbSettings("emulator-" + emulator.port)
     activities = []
-
-    
     
     file = open("activity_list", "w")
     file.write(api_commands.adb_get_activity_list(emulator, apk))
-    file.close();
+    file.close()
 
     file = open('activity_list', 'r')
     for l in file.readlines():
@@ -40,22 +38,35 @@ def some_method() -> str:
             activities.append(arr[1])
             print(arr[1])
 
+    result = api_commands.adb_display_properties().decode()
+    if 'DisplayDeviceInfo' in result:
+        arr = result.split('height=')[1]
+        display_height = arr.split(',')[0]
+
     for activity in activities:
+        previous_elements = []
         api_commands.adb_start_activity(emulator, apk, activity)
-        print(api_commands.adb_uiautomator_dump(emulator))
-        xmldoc = minidom.parse(api_commands.adb_uiautomator_dump(emulator))
-        element_list = []
-        itemlist = xmldoc.getElementsByTagName('node')
-        for s in itemlist:
-            if s.attributes['class'].value in ["android.widget.EditText"]:
-                bounds = s.attributes['bounds'].value.split("][")
-                Lpos = bounds[0][1:]
-                Rpos = bounds[1][:len(bounds[1])-1]
-                x1 = int (Lpos.split(",")[0])
-                y1 = int (Lpos.split(",")[1])
-                x2 = int (Rpos.split(",")[0])
-                y2 = int (Rpos.split(",")[1])
-                x = XML_Element(s.attributes['resource-id'].value,
+        element_list = test(emulator,adb_settings)
+       
+        while previous_elements != element_list:
+            previous_elements = element_list
+            api_commands.adb_display_scroll("{}".format(int(display_height) - int(display_height) / 10))
+            element_list = test(emulator,adb_settings)
+
+def test(emulator : Emulator, adb_settings : AdbSettings) -> List:
+    xmldoc = minidom.parse(api_commands.adb_uiautomator_dump(emulator))
+    element_list = []
+    itemlist = xmldoc.getElementsByTagName('node')
+    for s in itemlist:
+        if s.attributes['class'].value in ["android.widget.EditText"]:
+            bounds = s.attributes['bounds'].value.split("][")
+            Lpos = bounds[0][1:]
+            Rpos = bounds[1][:len(bounds[1]) - 1]
+            x1 = int(Lpos.split(",")[0])
+            y1 = int(Lpos.split(",")[1])
+            x2 = int(Rpos.split(",")[0])
+            y2 = int(Rpos.split(",")[1])
+            x = XML_Element(s.attributes['resource-id'].value,
                             s.attributes['class'].value,
                             s.attributes['checkable'].value,
                             s.attributes['checked'].value,
@@ -67,19 +78,21 @@ def some_method() -> str:
                             s.attributes['long-clickable'].value,
                             s.attributes['password'].value,
                             s.attributes['selected'].value,
-                            (x1+x2)/2,
-                            (y1+y2)/2)
-                element_list.append(x)
+                            (x1 + x2) / 2,
+                            (y1 + y2) / 2)
+            element_list.append(x)
 
-        for item in element_list:
-            print(item.resource_id, item.xpos, item.ypos)
-            api_commands.adb_input_tap(emulator, item.xpos, item.ypos)
-            rand = random.randint(5, 10)
-            for i in range(rand):
-                KeyCode = KeyboardEvent(random.randint(0, 40)).name
-                print("Sending event " + KeyCode)
-                adb_settings.adb_send_key_event_test(KeyCode)
-            adb_settings.adb_send_key_event_test("KEYCODE_BACK")
+    for item in element_list:
+        print(item.resource_id, item.xpos, item.ypos)
+        api_commands.adb_input_tap(emulator, item.xpos, item.ypos)
+        rand = random.randint(5, 10)
+        for i in range(rand):
+            KeyCode = KeyboardEvent(random.randint(0, 40)).name
+            print("Sending event " + KeyCode)
+            adb_settings.adb_send_key_event_test(KeyCode)
+        adb_settings.adb_send_key_event_test("KEYCODE_BACK")
 
-if __name__ == '__main__':
+    return element_list
+
+if __name__ == '__main__':    
     some_method()
