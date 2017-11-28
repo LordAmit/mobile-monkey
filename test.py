b67ed3a39e39b2a2745aca534aa35f29451a613c
Md. Mehedi Hasan <bsse0607@iit.du.ac.bd>
2017-11-28 18:06:07 +0600

    fix input event for hidden elements

--- test.py
+++ test.py
@@ -18,35 +18,57 @@ def some_method() -> str:
     apk = Apk(config.APK_FULL_PATH)
     emulator = emulator_manager.get_adb_instance_from_emulators(config.EMULATOR_NAME)
     adb_settings = AdbSettings("emulator-" + emulator.port)
-    activities = []
+    activities = ['com.datasoft.co_op360.presentation.login.LoginActiv']
     
-    file = open("activity_list", "w")
-    file.write(api_commands.adb_get_activity_list(emulator, apk))
-    file.close()
+    # file = open("activity_list", "w")
+    # file.write(api_commands.adb_get_activity_list(emulator, apk))
+    # file.close()
 
-    file = open('activity_list', 'r')
-    for l in file.readlines():
-        if 'A: android:name' in l and 'Activity' in l:
-            arr = l.split('"')
-            activities.append(arr[1])
-            print(arr[1])
+    # file = open('activity_list', 'r')
+    # for l in file.readlines():
+    #     if 'A: android:name' in l and 'Activity' in l:
+    #         arr = l.split('"')
+    #         activities.append(arr[1])
+    #         print(arr[1])
+    # print(len(activities))
 
-    result = api_commands.adb_display_properties().decode()
-    if 'DisplayDeviceInfo' in result:
-        arr = result.split('height=')[1]
+    display_properties = api_commands.adb_display_properties().decode()
+    if 'DisplayDeviceInfo' in display_properties:
+        arr = display_properties.split('height=')[1]
         display_height = arr.split(',')[0]
 
     for activity in activities:
-        previous_elements = []
         api_commands.adb_start_activity(emulator, apk, activity)
-        element_list = test(emulator,adb_settings)
-
-        while len(set(previous_elements).intersection(element_list)) >0 :
+        element_list = get_elements_list(emulator, adb_settings)
+        
+        while len(element_list) > 0 :
+            input_key_event(element_list,emulator,adb_settings)
             previous_elements = element_list
             api_commands.adb_display_scroll("{}".format(int(display_height) - int(display_height) / 10))
-            element_list = test(emulator,adb_settings)
+            element_list = get_elements_list(emulator,adb_settings)
+            element_list = element_list_compare(previous_elements,element_list)
 
-def test(emulator : Emulator, adb_settings : AdbSettings) -> List:
+def element_list_compare(previous_elements : XML_Element, current_elements : XML_Element):
+    for previous_item in previous_elements:
+        for current_item in current_elements:
+            if previous_item.resource_id == current_item.resource_id:
+                print('matched')
+                current_elements.remove(current_item)
+    return current_elements
+
+
+def input_key_event(element_list: XML_Element, emulator: Emulator, adb_settings: AdbSettings):
+    for item in element_list:
+        print(item.resource_id, item.xpos, item.ypos)
+        api_commands.adb_input_tap(emulator, item.xpos, item.ypos)
+        rand = random.randint(config.MINIMUM_KEYEVENT, config.MAXIMUM_KEYEVENT)
+        for i in range(rand):
+            KeyCode = KeyboardEvent(random.randint(0, 40)).name
+            print("Sending event " + KeyCode)
+            adb_settings.adb_send_key_event_test(KeyCode)
+        adb_settings.adb_send_key_event_test("KEYCODE_BACK")
+
+def get_elements_list(emulator : Emulator, adb_settings : AdbSettings) -> List:
     xmldoc = minidom.parse(api_commands.adb_uiautomator_dump(emulator))
     element_list = []
     itemlist = xmldoc.getElementsByTagName('node')
@@ -74,17 +96,6 @@ def test(emulator : Emulator, adb_settings : AdbSettings) -> List:
                             (x1 + x2) / 2,
                             (y1 + y2) / 2)
             element_list.append(x)
-
-    for item in element_list:
-        print(item.resource_id, item.xpos, item.ypos)
-        api_commands.adb_input_tap(emulator, item.xpos, item.ypos)
-        rand = random.randint(5, 10)
-        for i in range(rand):
-            KeyCode = KeyboardEvent(random.randint(0, 40)).name
-            print("Sending event " + KeyCode)
-            adb_settings.adb_send_key_event_test(KeyCode)
-        adb_settings.adb_send_key_event_test("KEYCODE_BACK")
-
     return element_list
 
 if __name__ == '__main__':    
