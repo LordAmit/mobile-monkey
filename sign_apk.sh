c1d1584862301f11f1bbc5f4786654c88628c98e
Amit Seal Ami <lordamit@gmail.com>
2018-05-24 22:21:49 +0600

    script for signing apk only

--- sign_apk.sh
+++ sign_apk.sh
@@ -1,3 +1,3 @@
-apk_path='/home/amit/git/testApps/notepad.apk'
+apk_path='/home/amit/git/testApps/org.wikipedia_232.apk'
 
 /home/amit/Android/Sdk/build-tools/27.0.3/apksigner sign --ks /home/amit/my.keystore $apk_path
